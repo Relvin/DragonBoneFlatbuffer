303ee201ff413ab4ba2ecb41c45087a9e4f84250
Relvin <xiakaiqiang@hotmail.com>
2016-03-25 10:57:51 +0800

    create new project

---
+++ tools/generate_code.sh
@@ -0,0 +1,4 @@
+./flatc -c --no-prefix -o ./xmlToBinary/xmlToBinary ./xmlToBinary/xmlToBinary/dragonbones.fbs
+
+cp ./xmlToBinary/xmlToBinary/dragonbones_generated.h ../../../../three_client/trunk/MegoGame/frameworks/runtime-src/Classes/dragonbones
+
